6eada07a41fbdb81b36f6cd8a6f86a2e74b19f47
unknown <benhall84707@gmail.com>
2022-02-13 18:28:38 -0500

    modified spacing

--- src/feature/StudentCard/TestResult.js
+++ src/feature/StudentCard/TestResult.js
@@ -4,11 +4,7 @@ const StyledResult = styled.div``;
 
 // I made this its own component incase TestResult grew to be more complicated.
 const TestResult = ({ result }) => {
-  return (
-    <StyledResult>
-      Test:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{result}%
-    </StyledResult>
-  );
+  return <StyledResult>Test:&ensp;&ensp;&ensp;{result}%</StyledResult>;
 };
 
 export default TestResult;
